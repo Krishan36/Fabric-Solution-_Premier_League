7a0fa6773ea218400fc3300180244441e06be9b6
Krishan36 <krishan95@hotmail.com>
2024-10-06 18:59:33 +0100

    Update Dim Home Team.sql

--- Premier League Dataset.Warehouse/dbo/Tables/Dim Home Team.sql
+++ Premier League Dataset.Warehouse/dbo/Tables/Dim Home Team.sql
@@ -6,3 +6,6 @@ CREATE TABLE [dbo].[Dim Home Team] (
 	[TeamColours] varchar(8000) NULL
 );
 
+
+GO
+ALTER TABLE [dbo].[Dim Home Team] ADD CONSTRAINT UQ_e8c4aa45_d62d_4aa8_ac2b_40ca2c1e6b40 unique NONCLUSTERED ([Home Team]);
