a65e3a9508db5d3e5f989b7111e8bc093c9db9eb
Krishan36 <krishan95@hotmail.com>
2024-10-06 18:56:56 +0100

    Update Dim Away Team.sql

--- Premier League Dataset.Warehouse/dbo/Tables/Dim Away Team.sql
+++ Premier League Dataset.Warehouse/dbo/Tables/Dim Away Team.sql
@@ -1,8 +1,10 @@
 CREATE TABLE [dbo].[Dim Away Team] (
-
 	[Away Team] varchar(8000) NULL, 
 	[ImageURL] varchar(8000) NULL, 
 	[Abbreviations] varchar(8000) NULL, 
 	[TeamColours] varchar(8000) NULL
 );
 
+
+GO
+ALTER TABLE [dbo].[Dim Away Team] ADD CONSTRAINT UQ_a2d64fb1_ee75_4ff2_a940_dd645b27f49e unique NONCLUSTERED ([Away Team]);
