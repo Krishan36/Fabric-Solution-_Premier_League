25948942740287631c47886bd48c659e435e70d0
Krishan36 <krishan95@hotmail.com>
2024-10-06 19:00:34 +0100

    Update Dim Match Number.sql

--- Premier League Dataset.Warehouse/dbo/Tables/Dim Match Number.sql
+++ Premier League Dataset.Warehouse/dbo/Tables/Dim Match Number.sql
@@ -3,3 +3,6 @@ CREATE TABLE [dbo].[Dim Match Number] (
 	[Match Number] bigint NULL
 );
 
+
+GO
+ALTER TABLE [dbo].[Dim Match Number] ADD CONSTRAINT UQ_30a3d84c_152a_48fe_84bd_93fe44882e5f unique NONCLUSTERED ([Match Number]);
