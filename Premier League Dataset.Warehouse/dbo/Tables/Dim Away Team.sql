0cc4bdf55575eb0bac64d01a6bd38fa60d530128
Krishan36 <krishan95@hotmail.com>
2024-10-06 19:11:13 +0100

    Update Dim Away Team.sql

--- Premier League Dataset.Warehouse/dbo/Tables/Dim Away Team.sql
+++ Premier League Dataset.Warehouse/dbo/Tables/Dim Away Team.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [dbo].[Dim Away Team] (
+
 	[Away Team] varchar(8000) NULL, 
 	[ImageURL] varchar(8000) NULL, 
 	[Abbreviations] varchar(8000) NULL, 
 	[TeamColours] varchar(8000) NULL
 );
-
 
 
 GO
